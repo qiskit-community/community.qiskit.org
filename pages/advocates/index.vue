301dc01b82e0bb217be473a1764defcd129ba216
SooluThomas <soolu.elto@gmail.com>
2019-07-18 10:35:41 -0400

    Moved from layouts to pages

--- pages/advocates/index.vue
+++ pages/advocates/index.vue
@@ -1,5 +1,67 @@
 <template>
   <main>
+    <section id="whyJoin" class="join">
+      <h3 class="section-title">
+        Why Join
+      </h3>
+      <div class="gallery-text">
+        <div class="gallery-text-column">
+          <div class="gallery-text-content">
+            <img src="images/icons/apply01.svg">
+            <div>
+              <h3 class="gallery-text-subtitle">
+                Funding for your projects and work
+              </h3>
+              <p>Advocates can request funding or events and projects</p>
+            </div>
+          </div>
+          <div class="gallery-text-content">
+            <img src="images/icons/apply03.svg">
+            <div>
+              <h3 class="gallery-text-subtitle">
+                Prioritized access to hardware
+              </h3>
+              <p>Advocates will receive prioritized access to publicly available hardware</p>
+            </div>
+          </div>
+        </div>
+        <div class="gallery-text-column">
+          <div class="gallery-text-content">
+            <img src="images/icons/apply02.svg">
+            <div>
+              <h3 class="gallery-text-subtitle">
+                Network with experts and enthusiasts
+              </h3>
+              <p>Advocates will be added to a group of quantum experts and will receive ~~~~(??).</p>
+            </div>
+          </div>
+          <div class="gallery-text-content">
+            <img src="images/icons/apply04.svg">
+            <div>
+              <h3 class="gallery-text-subtitle">
+                Increased visibility for your work
+              </h3>
+              <p>All advocates will have the opportunity to have their work supported by IBM</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="apply">
+      <h3 class="section-title">
+        Steps to apply
+      </h3>
+      <ol>
+        <li>Fill the form below</li>
+        <li>Click the link to attend test</li>
+        <li>Learn, Do the test and get certified!</li>
+      </ol>
+      <div class="button-container">
+        <button onclick="alert('Redirect to apply form')">
+          Apply Now!
+        </button>
+      </div>
+    </section>
     <section
       v-for="(section, index) in sections"
       :key="`section-${index}`"
