563aa183371bb1c84554836412a0db144badd024
Salvador de la Puente González <salva@unoyunodiez.com>
2019-08-13 15:05:13 +0200

    Finish the content area featuring separators by color and line

--- layouts/education.vue
+++ layouts/education.vue
@@ -10,7 +10,7 @@
           <iframe
             width="560"
             height="315"
-            src="https://www.youtube.com/embed/a1NZC5rqQD8"
+            src="https://www.youtube.com/embed/_EweJlth2Sk"
             frameborder="0"
             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen
@@ -18,12 +18,84 @@
           <div>
             <h1>Qiskit for Educators</h1>
             <p>Qiskit makes it easy to start learning quantum software to run on real quantum hardware. Teach your students with the same tools used by scientists and engineers worldwide to accelerate research towards practical applications for quantum computing.</p>
-            <ul>
+            <ul class="actions">
               <li><a class="button" href="#">Contact Us</a></li>
             </ul>
           </div>
         </section>
       </header>
+      <section id="highlights" class="content">
+        <section class="feature">
+          <img src="/images/education/event.svg" />
+          <h2>Attend Exclusive Events</h2>
+          <p>Lorem ipsum dolor amet...</p>
+        </section>
+        <section class="feature">
+          <img src="/images/education/event.svg" />
+          <h2>Join a Global Community</h2>
+          <p>Lorem ipsum dolor amet...</p>
+        </section>
+        <section class="feature">
+          <img src="/images/education/event.svg" />
+          <h2>Teach with Live Demos</h2>
+          <p>Lorem ipsum dolor amet...</p>
+        </section>
+      </section>
+      <section id="video-series">
+        <div class="content">
+          <div class="description">
+            <h2>Coding With Qiskit Video Series</h2>
+            <p>
+              Accompany Abraham Asfaw through a series of video tutorials
+              in our YouTube Channel explaining quantum computing through
+              the use of Qiskit.
+            </p>
+            <ul class="actions">
+              <li>
+                <a class="button" href="https://www.youtube.com/watch?v=a1NZC5rqQD8">
+                  Last Episode: Installing Qiskit
+                </a>
+              </li>
+              <li>
+                <a class="button" href="https://www.youtube.com/playlist?list=PLOFEBzvs-Vvp2xg9-POLJhQwtVktlYGbY">
+                  All the episodes
+                </a>
+              </li>
+            </ul>
+          </div>
+          <aside class="episode">
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/M4EkW4VwhcI"
+              frameborder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            />
+          </aside>
+        </div>
+      </section>
+      <section id="textbook">
+        <div class="content">
+          <aside />
+          <div class="description">
+            <h2>Qiskit Textbook</h2>
+            <p>
+              Leverage the power of quantum computing using Qiskit with this
+              university course supplement covering introductory materials,
+              advanced algorithms and hardware. Include problems and
+              exercises.
+            </p>
+            <ul class="actions">
+              <li>
+                <a class="button" href="/textbook/">
+                  Go to index
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
     </main>
     <footer>footer</footer>
   </div>
@@ -33,8 +105,17 @@
 @import url(~/static/css/fonts.css);
 
 :root {
-  --dark-color: #242a2e;
+  --ibm-blue: #0a1d8f;
+  --primary-color: #242a2e;
+  --primary-color-dark: #161f25;
+  --primary-color-darkmost: #0d1a22;
+  --primary-color-light: #424c53;
+  --primary-color-lightmost: #5d6870;
   --secondary-color: #893ffc;
+  --secondary-color-lightmost: #bc93fc;
+  --secondary-color-dark: #6f16fa;
+  --secondary-color-darkmost: #6105f2;
+  --body-color-light: #e0e0e0;
 }
 
 * {
@@ -47,7 +128,7 @@
 }
 
 html {
-  background-color: var(--dark-color);
+  background-color: var(--primary-color);
 }
 
 html,
@@ -56,6 +137,18 @@ body,
 #__layout {
   height: 100%;
   min-height: 100%;
+}
+
+h1 {
+  font-size: 2.2rem;
+}
+
+h2 {
+  font-size: 1.2rem;
+}
+
+p {
+  margin-top: 1.5rem;
 }
 
 .content-root {
@@ -73,11 +166,12 @@ body,
   width: 100%;
   max-width: 1040px;
   margin: 0 auto;
+  padding: 0 1rem;
 }
 
 .button {
   padding: 0.66rem 1rem;
-  background-color: var(--dark-color);
+  background-color: var(--primary-color);
   border: 2px solid var(--secondary-color);
   font-size: 0.75em;
   font-weight: 100;
@@ -86,6 +180,7 @@ body,
   text-decoration: none;
   box-shadow: 0 4px 6px rgba(50,50,93,.11),0 1px 3px rgba(0,0,0,.08);
   white-space: nowrap;
+  line-height: 4rem;
 }
 
 .global-nav {
@@ -96,13 +191,20 @@ body,
   height: 50px;
   border: none;
   border-bottom: 1px solid black;
-  background-color: var(--dark-color);
+  background-color: var(--primary-color);
   z-index: 100;
+}
+
+main {
+  color: var(--body-color-light);
+  background-color: var(--primary-color-darkmost);
+  background-image: linear-gradient(150deg, var(--primary-color-darkmost) 15%,var(--primary-color-dark) 70%,var(--primary-color) 94%);
 }
 
 main > header {
   position: relative;
   top: 50px;
+  z-index: 1;
 }
 
 .gates {
@@ -113,7 +215,6 @@ main > header {
   right: 0;
   height: 100%;
   background-image: linear-gradient(150deg,#893ffc 15%,#a167fc 70%,#bc93fc 94%);
-  z-index: -1;
 }
 
 #intro {
@@ -133,18 +234,113 @@ main > header {
   min-width: 560px;
   max-width: 560px;
   height: 315px;
+  box-shadow: 0 13px 27px -5px rgba(50,50,93,.25),
+              0 8px 16px -8px rgba(0,0,0,.5),
+              0 -6px 16px -6px rgba(0,0,0,.025);
 }
 
-#intro p {
+.actions {
+  list-style: none;
   margin-top: 1.5rem;
 }
 
-#intro ul {
-  list-style: none;
-  margin-top: 2.5rem;
+.actions li {
+  display: inline-block;
+  margin-right: 1rem;
 }
 
-#intro ul li {
-  display: inline-block;
+.actions li:last-child {
+  margin-right: 0;
+}
+
+#video-series {
+  margin-top: 5rem;
+  margin-bottom: 4rem;
+}
+
+#video-series .content {
+  display: flex;
+  padding-top: 5rem;
+  border-top: 2px solid var(--primary-color);
+}
+
+.episode {
+  margin-left: 2rem;
+  transform: perspective(1200px) rotateY(-20deg) rotateX(5deg);
+}
+
+.episode iframe {
+  border-radius: 10px;
+  box-shadow: 0 10px 30px 0 #000000b0;
+}
+
+#textbook {
+  color: #333;
+  background-color: white;
+  background-image: linear-gradient(to right, #f2f4f8 0, #f2f4f8 50%, white 50.01%);
+}
+
+#textbook aside {
+  background-image: url('/images/education/illustration-ibmq-experience.png');
+  background-repeat: no-repeat;
+  background-size: contain;
+}
+
+#textbook .content {
+  display: flex;
+}
+
+#textbook .content > * {
+  flex: 1;
+  box-sizing: border-box;
+}
+
+#textbook .description {
+  background-color: white;
+  padding: 6rem 0 4rem 2rem;
+}
+
+#highlights {
+  display: flex;
+  flex-direction: row;
+  padding-top: 8rem;
+}
+
+#highlights .feature {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.feature {
+  box-sizing: border-box;
+  border: 4px solid var(--secondary-color);
+  border-radius: 10px;
+  margin: 0 1rem 0 0;
+  padding: 2rem 1rem;
+}
+
+.feature:last-child {
+  margin-right: 0;
+}
+
+.feature img {
+  width: 20%;
+  padding-bottom: 2rem;
+}
+
+.feature h2 {
+  font-size: 1.1rem;
+  text-align: center;
+}
+
+.feature p {
+  text-align: center;
+  margin-top: 1rem;
+}
+
+footer {
+  margin-top: 8rem;
 }
 </style>
