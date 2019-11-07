b950ab84793c36181b8694e6b7fcb7bd24e78249
y4izus <yaiza.garcia.mm@gmail.com>
2019-11-07 12:15:54 +0100

    Creates a new component for SidebarMenu

--- components/menus/QiskitOrgMenu.vue
+++ components/menus/QiskitOrgMenu.vue
@@ -2,39 +2,7 @@
   <section>
     <div class="menu-container">
       <div class="menu menu--framed">
-        <section class="drawer" tabindex="-1">
-          <svg class="drawer-toggle" height="24" viewBox="0 0 24 24" width="24">
-            <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
-          </svg>
-          <div class="overlay" />
-          <nav class="vertical-navigation">
-            <h2>Elements</h2>
-            <a 
-            v-for="element in elements" 
-            :key="element.label" 
-            class="vertical-navigation__item" 
-            :href="element.url">
-              {{element.label}}
-            </a>
-            <h2>Learn more</h2>
-            <a class="vertical-navigation__item vertical-navigation__item--active" href="/">Community</a>
-            <div class="vertical-community-navigation">
-              <nuxt-link
-                v-for="(link, index) in links"
-                :key="index"
-                :class="{
-                  'vertical-community-navigation__item': true,
-                  'nuxt-link-active': isActive(link.to)
-                }"
-                :to="link.to"
-              >
-                {{ link.label }}
-              </nuxt-link>
-            </div>
-            <a class="vertical-navigation__item" href="https://quantum-computing.ibm.com/jupyter/tutorial/1_start_here.ipynb" target="_blank">Tutorials</a>
-            <a class="vertical-navigation__item" href="https://qiskit.org/documentation">API&nbsp;Documentation</a>
-          </nav>
-        </section>
+        <SidebarMenu/>
         <a class="link-to-home" :href="baseUrl">Qiskit</a>
         <nav class="navigation-group navigation-group--with-separator">
           <a 
@@ -75,10 +43,14 @@
 <script lang="ts">
 import Vue from 'vue'
 import { Component, Prop } from 'vue-property-decorator'
+import SidebarMenu from '~/components/menus/SidebarMenu.vue'
 
 import { QISKIT_URL, orderedQiskitElements } from '~/constants/links'
 
-@Component
+
+@Component({
+  components: { SidebarMenu }
+})
 export default class extends Vue {
   @Prop({
     type: Array,
@@ -99,13 +71,6 @@ export default class extends Vue {
 
 <style lang="scss" scoped>
 @import '~/assets/scss/mixins.scss';
-
-@mixin vertical-navigation-item() {
-  font-size: 0.9rem;
-  text-decoration: none;
-  color: white;
-  padding: 0.5rem 1.5em;
-}
 
 * {
   box-sizing: border-box;
@@ -191,82 +156,6 @@ export default class extends Vue {
   text-decoration: none;
 }
 
-.drawer {
-  display: none;
-}
-
-.drawer-toggle {
-  fill: white;
-  height: 100%;
-  cursor: pointer;
-  margin: 0 0 0 1.5rem;
-}
-
-.overlay {
-  position: fixed;
-  top: 0; right: 0; bottom: 0; left: 0;
-  z-index: 150;
-  background-color: black;
-  opacity: 0;
-  transition: opacity 200ms;
-  pointer-events: none;
-}
-
-.vertical-navigation {
-  display: flex;
-  flex-direction: column;
-  position: fixed;
-  top: 0; bottom: 0; left: 0;
-  z-index: 200;
-  width: 256px;
-  padding: 1.3rem;
-  background-color: var(--primary-color);
-  transform: translateX(-100%);
-  transition: transform 200ms;
-  overflow-y: auto;
-
-  h2 {
-    font-size: 0.8rem;
-    font-weight: normal;
-    color: var(--primary-color-lightmost);
-    padding: 1em;
-  }
-
-  &__item {
-    @include vertical-navigation-item();
-  }
-}
-
-.vertical-community-navigation {
-  display: flex;
-  flex-direction: column;
-  margin: 0 -1.3rem;
-  padding: 1rem 0;
-  background-color: var(--secondary-color-lightmost);
-
-  &__item {
-    @include vertical-navigation-item();
-    padding: 0.5rem 3rem;
-    color: var(--body-color-dark);
-
-    &.nuxt-link-active {
-      font-weight: bold;
-      border-left: 4px solid var(--secondary-color);
-      padding-left: calc(3rem - 4px);
-    }
-  }
-}
-
-.drawer:focus-within {
-  .vertical-navigation {
-    transform: translateX(0);
-  }
-
-  .overlay {
-    opacity: 0.5;
-  }
-}
-
 @media (max-width: 830px) {
 
   #navigation {
@@ -276,11 +165,6 @@ export default class extends Vue {
   .link-to-home {
     font-size: 1.1rem;
     margin-left: -0.5rem;
-  }
-
-  .drawer {
-    display: unset;
-    margin-left: -2rem;
   }
 
   .community-menu,
