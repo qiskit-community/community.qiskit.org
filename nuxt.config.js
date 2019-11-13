c091e13527089b5a8642845d42303c71b9fa4673
Salvador de la Puente González <salva@unoyunodiez.com>
2019-11-13 16:45:20 +0100

    Add hotjar code snippet

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,6 +57,18 @@ export default {
       {
         src: 'https://cloud.ibm.com/analytics/build/bluemix-analytics.min.js',
         async: true
+      },
+      {
+        hid: 'hotjar',
+        innerHTML: `
+(function(h,o,t,j,a,r){
+  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+  h._hjSettings={hjid:42920,hjsv:6};
+  a=o.getElementsByTagName('head')[0];
+  r=o.createElement('script');r.async=1;
+  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+  a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`
       }
     ],
     meta: [
@@ -68,7 +80,8 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ],
     __dangerouslyDisableSanitizersByTagID: {
-      'segment': ['innerHTML']
+      'segment': ['innerHTML'],
+      'hotjar': ['innerHTML']
     }
   },
 
