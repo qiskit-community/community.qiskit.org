46e2779bfc75d7636d9b3efb55e7c4f02d1c500d
Salvador de la Puente González <neo.salvador@gmail.com>
2019-11-25 16:36:55 +0100

    Update hotjar id for qiskit.org
    Co-Authored-By: Marina Aisa <marina@marinaaisa.com>

--- plugins/hotjar.ts
+++ plugins/hotjar.ts
@@ -9,7 +9,7 @@ export default () => {
   /* eslint-disable */
   (function(h,o,t,j,a?: any,r?: any){
     h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-    h._hjSettings={hjid:42920,hjsv:6};
+    h._hjSettings={hjid:1586421,hjsv:6};
     a=o.getElementsByTagName('head')[0];
     r=o.createElement('script');r.async=1;
     r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
