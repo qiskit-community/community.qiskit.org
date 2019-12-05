e5b8c88daf7ec7dab448b0817b59d881d3176032
y4izus <yaiza.garcia.mm@gmail.com>
2019-12-05 13:50:07 +0100

    Fix lint

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,7 @@ md.use(miLinkAttributes, {
   }
 })
 md.use(miAnchor, {
-  slugify (id) { return uslug(id) }
+  slugify(id) { return uslug(id) }
 })
 
 export default {
@@ -98,7 +98,7 @@ export default {
     /*
     ** You can extend webpack config here
     */
-    extend (config) {
+    extend(config) {
       config.module.rules.push({
         test: /\.md$/,
         loader: 'frontmatter-markdown-loader',
@@ -123,7 +123,7 @@ export default {
   },
 
   router: {
-    scrollBehavior (to) {
+    scrollBehavior(to) {
       if (to.hash) {
         const el = document.querySelector(to.hash)
         if ('scrollBehavior' in document.documentElement.style) {
@@ -152,7 +152,7 @@ export default {
 
   hooks: {
     build: {
-      before () {
+      before() {
         generateTextbookToc(
           './static/textbook/preface.html',
           './content/education/textbook-toc.md'
